d3f59fa5caef7975b038d79e07c6a241c0ef6a54
Giwan Kim <giwan.kim@gmail.com>
2021-08-26 11:54:52 +0900

    feat(bowling): Add NormalFrame

---
+++ src/test/java/bowling/domain/NormalFrameTest.java
@@ -0,0 +1,18 @@
+package bowling.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import bowling.exception.InvalidNormalFrameException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NormalFrameTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10})
+    void of_invalidFrameNumber(int frameNumber) {
+        assertThatThrownBy(() -> NormalFrame.of(frameNumber))
+                .isInstanceOf(InvalidNormalFrameException.class);
+    }
+
+}
